e9669e1819760122a4d62b7d921283d2d46ff819
Leandro Doctors <ldoctors@gmail.com>
2016-03-10 12:48:07 -0300

    Add "AgentTest" class

---
+++ src/test/scala/ar/com/allentiak/portfolio_manager/AgentTest.scala
@@ -0,0 +1,12 @@
+package ar.com.allentiak.portfolio_manager
+
+import org.scalatest._
+
+class AgentTest extends FunSpec {
+
+  describe("an Agent") {
+    it("should have at least one buying strategy") (pending)
+    it("should have at least one selling strategy") (pending)
+  }
+
+}
