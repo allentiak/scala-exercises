ddfde578b872784d711912fa834232821dfd2326
Leandro Doctors <ldoctors@gmail.com>
2016-03-15 16:37:23 -0300

    Strategy should be abstract

--- src/main/scala/ar/com/allentiak/portfolio_manager/Strategy.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Strategy.scala
@@ -1,6 +1,6 @@
 package ar.com.allentiak.portfolio_manager
 
-class Strategy(name: String)
+abstract class Strategy(name: String)
 
 case object BuyingStrategy extends Strategy("myBuyingStrategy")
 
