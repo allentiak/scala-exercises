6309b635f337bb95f6c51ee0b279d209f43ae7f0
Leandro Doctors <ldoctors@gmail.com>
2016-03-21 14:57:57 -0300

    Refactor "Action"

--- src/main/scala/ar/com/allentiak/portfolio_manager/Action.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Action.scala
@@ -1,13 +1,3 @@
 package ar.com.allentiak.portfolio_manager
 
-abstract class Action(name: String)
-
-case object YPFD extends Action("YPF, S.A.")
-
-case object AERO extends Action("Aerolíneas Argentinas, S.A.")
-
-case object BNA extends Action("Banco de la Nación Argentina, S.E.")
-
-case object SANCOR extends Action("SanCor, Coop. Ltda.")
-
-case object ARCOR extends Action("Arcor, S.A.")
+case class Action(name: String = "Sample Action Name", shortname: String = "SAMPLE")
