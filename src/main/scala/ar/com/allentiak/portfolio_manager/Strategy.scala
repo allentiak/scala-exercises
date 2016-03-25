82899add26b7c10653b80cd0ba080a8656e68294
Leandro Doctors <ldoctors@gmail.com>
2016-03-25 20:30:15 -0300

    Implement strategies

--- src/main/scala/ar/com/allentiak/portfolio_manager/Strategy.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Strategy.scala
@@ -1,9 +1,34 @@
 package ar.com.allentiak.portfolio_manager
 
-abstract class Strategy{
-  def analyseMarket:Decision
+abstract class Strategy {
+  def analyse(action: Action): Decision
 }
 
-case object ShortTermStrategy extends Strategy
+case object ShortTermStrategy extends Strategy {
 
-case object LongTertmStrategy extends Strategy
+  override def analyse(action: Action) = {
+    val todaysprice: Double = action.lastprice
+    val yesterdaysprice: Double = action.prices.tail.head
+    val difference = (todaysprice - yesterdaysprice) / yesterdaysprice
+
+    difference match {
+      case difference if (difference >= 0.02) => Sell
+      case difference if (difference <= -0.01) => Buy
+      case _ => Ignore
+    }
+  }
+}
+
+case object LongTermStrategy extends Strategy {
+  override def analyse(action: Action) = {
+    val todaysprice: Double = action.lastprice
+    val yesterdaysprice: Double = action.prices.tail.head
+    val difference = (todaysprice - yesterdaysprice) / yesterdaysprice
+
+    difference match {
+      case difference if (action.prices.size > 5) => Sell
+      case difference if ((difference <= -0.01) || (todaysprice >= (action.averageprice *2))) => Buy
+      case _ => Ignore
+    }
+  }
+}
