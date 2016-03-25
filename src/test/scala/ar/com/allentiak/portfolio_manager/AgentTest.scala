452ad2f65dbe773354cb134544ca8bb1e4da58d6
Leandro Doctors <ldoctors@gmail.com>
2016-03-25 20:45:03 -0300

    Overhaul AgentTest (does not work yet)

--- src/test/scala/ar/com/allentiak/portfolio_manager/AgentTest.scala
+++ src/test/scala/ar/com/allentiak/portfolio_manager/AgentTest.scala
@@ -2,21 +2,34 @@ package ar.com.allentiak.portfolio_manager
 
 import org.scalatest._
 
-class AgentTest extends FunSpec {
+object AgentTest extends FunSpec {
 
-  describe("an Agent") {
-    val myAgent = new Agent
+  describe("Given a Market") {
+    val myAction1
+    val myAction2
+    val myMarket:Market = new Market("Test Market", actions: scala.collection.immutable.Set[ar.com.allentiak.portfolio_manager.Action])
+    val myAgent = new Agent(name="Smith")
+    it("should monitor a market")(pending)
 
-    it("should (by default) have a non-null name") {
-      assert(myAgent.name !== Nil)
+    describe("An Agent with a \"Short Term\" Strategy") {
+
+      val myShortTermStrategy: Strategy = ShortTermStrategy
+
+      it("should buy an action if the price dropped at least 1% from the day before") {
+        // conditions here
+        assert(myShortTermStrategy.decision == Sell)
+      }
+
+      it("should sell an action if the price rose at least 2% from the day before")(pending)
     }
-    it("should (by default) have a non-null strategy") {
-      assert(myAgent.strategy.isDefined)
-    }
-    it("should (by default) have a non-null set of actions") {
-      assert(myAgent.portfolio.isDefined)
+
+    describe("An Agent with a \"Long-Term\" Strategy") {
+      val myLongTimeStrategy: Strategy = LongTermStrategy
+
+      it("should buy an action if the price dropped at least 1% from the day before, or if the price is at least twice the value of the average historical price")(pending)
+
+      it("should sell an action after five days")(pending)
     }
 
   }
-
 }
