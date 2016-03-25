1c9d0866a63b21e8322ff7a5b712f3d0f3740731
Leandro Doctors <ldoctors@gmail.com>
2016-03-25 16:55:02 -0300

    Fix test nesting (undo f6d6f5e0)

--- src/test/scala/ar/com/allentiak/portfolio_manager/AgentTest.scala
+++ src/test/scala/ar/com/allentiak/portfolio_manager/AgentTest.scala
@@ -7,17 +7,16 @@ class AgentTest extends FunSpec {
   describe("an Agent") {
     val myAgent = new Agent
 
-    it("by default") {
-      it("should have a non-null name") {
-        assert(myAgent.name !== Nil)
-      }
-      it("should have a non-null strategy") {
-        assert(myAgent.strategy.isDefined)
-      }
-      it("should have a non-null set of actions") {
-        assert(myAgent.portfolio.isDefined)
-      }
+    it("should (by default) have a non-null name") {
+      assert(myAgent.name !== Nil)
     }
+    it("should (by default) have a non-null strategy") {
+      assert(myAgent.strategy.isDefined)
+    }
+    it("should (by default) have a non-null set of actions") {
+      assert(myAgent.portfolio.isDefined)
+    }
+
   }
 
 }
