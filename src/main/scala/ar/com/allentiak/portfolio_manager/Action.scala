c74775bee38fcd24c04fd0ca0054da28a0e2aa14
Leandro Doctors <ldoctors@gmail.com>
2016-03-28 14:07:58 -0300

    Overhaul Strategies

--- src/main/scala/ar/com/allentiak/portfolio_manager/Action.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Action.scala
@@ -1,9 +1,33 @@
 package ar.com.allentiak.portfolio_manager
 
-import scala.collection.immutable.Stack
+import scala.collection.immutable.List
 
-case class Action(name: String, shortname: String, initialprice: Double, prices: Stack[Double] = Stack.empty) {
-  prices.push(initialprice)
-  def lastprice: Double = { prices.head }
-  def averageprice: Double = { prices.reduceLeft(_ + _)/prices.size }
+case class Action(name: String, shortname: String, initialprice: Double) {
+  val prices = List(initialprice)
+
+  def lastprice: Option[Double] = {
+    Some(prices.head)
+  }
+
+  def averageprice: Option[Double] = {
+    prices match {
+      case Nil => None
+      case _ => Some(prices.reduceLeft(_ + _) / prices.size)
+    }
+  }
+
+  def todaysprice: Option[Double] = {
+    prices match {
+      case Nil => None
+      case _ => Some(prices.head)
+    }
+  }
+
+  def yesterdaysprice: Option[Double] = Some(prices.tail.head)
+
+  def difference:Option[Double] = {
+    if (todaysprice.isDefined && yesterdaysprice.isDefined) Some((todaysprice.get - yesterdaysprice.get) / yesterdaysprice.get)
+    else None
+  }
+
 }
--- src/main/scala/ar/com/allentiak/portfolio_manager/Strategy.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Strategy.scala
@@ -5,30 +5,21 @@ abstract class Strategy {
 }
 
 case object ShortTermStrategy extends Strategy {
-
   override def analyse(action: Action) = {
-    val todaysprice: Double = action.lastprice
-    val yesterdaysprice: Double = action.prices.tail.head
-    val difference = (todaysprice - yesterdaysprice) / yesterdaysprice
-
-    difference match {
-      case difference if (difference >= 0.02) => Sell
-      case difference if (difference <= -0.01) => Buy
-      case _ => Ignore
+    action.difference match {
+      case Some(diff) if (diff >= 0.02) => Sell
+      case Some(diff) if (diff <= -0.01) => Buy
+      case None => Ignore
     }
   }
 }
 
 case object LongTermStrategy extends Strategy {
   override def analyse(action: Action) = {
-    val todaysprice: Double = action.lastprice
-    val yesterdaysprice: Double = action.prices.tail.head
-    val difference = (todaysprice - yesterdaysprice) / yesterdaysprice
-
-    difference match {
-      case difference if (action.prices.size > 5) => Sell
-      case difference if ((difference <= -0.01) || (todaysprice >= (action.averageprice *2))) => Buy
-      case _ => Ignore
+    action.difference match {
+      case Some(diff) if (action.prices.size > 5) => Sell
+      case Some(diff) if ((diff <= -0.01) || (action.todaysprice.get >= (action.averageprice.get * 2))) => Buy
+      case None => Ignore
     }
   }
 }
