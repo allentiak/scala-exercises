3f71dee4c1e2ec6fed66e229626272e7419f73c3
Leandro Doctors <ldoctors@gmail.com>
2016-03-28 17:11:30 -0300

    Agent: regen_portfolio (stub)

--- src/main/scala/ar/com/allentiak/portfolio_manager/Agent.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Agent.scala
@@ -1,3 +1,5 @@
 package ar.com.allentiak.portfolio_manager
 
-case class Agent(name: String, availablemoney: Double = 1000000, strategy: Option[Strategy] = None, portfolio: Option[Map[String, Double]] = None)
+case class Agent(name: String, availablemoney: Double = 1000000, strategy: Option[Strategy] = None, portfolio: Option[Map[String, Double]] = None) {
+  def regen_portfolio(market: Market, strategy: Strategy, portfolio: Option[Map[String, Double]]): Option[Map[String, Double]] = ???
+}
