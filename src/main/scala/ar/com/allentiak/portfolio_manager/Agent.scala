af5dc6220eb10a1637ff041119788ff7cf1db9db
Leandro Doctors <ldoctors@gmail.com>
2016-03-28 17:27:48 -0300

    Agent: use vars <- vals

--- src/main/scala/ar/com/allentiak/portfolio_manager/Agent.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Agent.scala
@@ -1,5 +1,5 @@
 package ar.com.allentiak.portfolio_manager
 
-case class Agent(name: String, availablemoney: Double = 1000000, strategy: Option[Strategy] = None, portfolio: Option[Map[String, Double]] = None) {
+case class Agent(name: String, var availablemoney: Double = 1000000, var strategy: Option[Strategy] = None, var portfolio: Option[Map[String, Double]] = None) {
   def regen_portfolio(market: Market, strategy: Strategy, portfolio: Option[Map[String, Double]]): Option[Map[String, Double]] = ???
 }
--- src/main/scala/ar/com/allentiak/portfolio_manager/Portfolio.scala
+++ src/main/scala/ar/com/allentiak/portfolio_manager/Portfolio.scala
@@ -1,3 +1,3 @@
 package ar.com.allentiak.portfolio_manager
 
-case class Portfolio(name: String, actions: Option[Map[Action, Integer]])
+case class Portfolio(name: String, var actions: Option[Map[Action, Integer]])
