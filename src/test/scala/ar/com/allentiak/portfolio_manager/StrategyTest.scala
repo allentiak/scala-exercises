4fdb4a7ba7038b3a88ed7864223c3c0ea07637a7
Leandro Doctors <ldoctors@gmail.com>
2016-03-30 14:03:22 -0300

    Formatting

--- src/test/scala/ar/com/allentiak/portfolio_manager/StrategyTest.scala
+++ src/test/scala/ar/com/allentiak/portfolio_manager/StrategyTest.scala
@@ -11,7 +11,7 @@ class StrategyTest extends FunSpec with Matchers {
     val raisingPrices = List(5.0, 4.8, 4.8, 4.7, 4.5, 4.0, 3.7)
     val droppingPrices = raisingPrices.reverse
     val stablePrices = List(2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0)
-    val slowlyDroppingPrices = List(1000.0,999.0)
+    val slowlyDroppingPrices = List(1000.0, 999.0)
     val slowlyRaisingPrices = slowlyDroppingPrices.reverse
 
     val raisingAction = Action(name = "RaisingAction", shortname = "UP", prices = raisingPrices)
@@ -20,15 +20,14 @@ class StrategyTest extends FunSpec with Matchers {
     val slowlyRaisingAction = Action(name = "SlowlyRaisingAction", shortname = "UPSLOW", prices = slowlyRaisingPrices)
     val slowlyDroppingAction = Action(name = "SlowlyDroppingAction", shortname = "DOWNSLOW", prices = slowlyDroppingPrices)
 
-
     it("should buy an action if the price dropped at least 1% from the day before") {
-      myShortTermStrategy.analyse(droppingAction) shouldBe a [Buy]
-      myShortTermStrategy.analyse(slowlyDroppingAction) shouldBe an [Ignore]
+      myShortTermStrategy.analyse(droppingAction) shouldBe a[Buy]
+      myShortTermStrategy.analyse(slowlyDroppingAction) shouldBe an[Ignore]
     }
 
     it("it should sell an action if the price rose at least 2% from the day before") {
-      myShortTermStrategy.analyse(raisingAction) shouldBe a [Sell]
-      myShortTermStrategy.analyse(slowlyRaisingAction) shouldBe an [Ignore]
+      myShortTermStrategy.analyse(raisingAction) shouldBe a[Sell]
+      myShortTermStrategy.analyse(slowlyRaisingAction) shouldBe an[Ignore]
     }
 
     it("should ignore an action if its price is stable") {
