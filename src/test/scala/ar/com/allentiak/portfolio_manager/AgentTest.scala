3ac9b35025d00681ef647b23e07b5b09cb25a0f1
Leandro Doctors <ldoctors@gmail.com>
2016-03-30 14:41:20 -0300

    Add Agent tests (stubs)

---
+++ src/test/scala/ar/com/allentiak/portfolio_manager/AgentTest.scala
@@ -0,0 +1,14 @@
+package ar.com.allentiak.portfolio_manager
+
+import org.scalatest.FunSpec
+
+class AgentTest extends FunSpec {
+
+  describe("An Agent") {
+    it("should follow a given market")(pending)
+    it("should have an initial ammount to invest")(pending)
+    it("should be able to drive its investmets by different strategies")(pending)
+    it("should be able to use its money to invest on a given market with all of them")(pending)
+    it("should be able to compare the results from each of his strategies and find the best strategy for a given period") (pending)
+  }
+}
