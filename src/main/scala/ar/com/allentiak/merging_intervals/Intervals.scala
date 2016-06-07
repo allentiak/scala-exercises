d9b8a9d890f7bcba144eefcb18efd4ae30dd3598
Leandro Doctors <ldoctors@gmail.com>
2016-06-06 23:33:39 -0300

    Interval: add function mergeWith

--- src/main/scala/ar/com/allentiak/merging_intervals/Intervals.scala
+++ src/main/scala/ar/com/allentiak/merging_intervals/Intervals.scala
@@ -1,6 +1,14 @@
 package ar.com.allentiak.merging_intervals
+import math.{max,min}
 
 case class Interval(lowerLimit:Int,higherLimit:Int)
 {
   def fitsInto(other:Interval):Boolean=(this.lowerLimit >= other.lowerLimit && this.higherLimit <= other.higherLimit)
+
+  def mergeWith(other:Interval):Interval={
+    if (this.fitsInto(other) || other.fitsInto(this))
+    Interval(min(this.lowerLimit,other.lowerLimit),max(this.higherLimit,other.higherLimit))
+    else null
+    // Totally unclean, but I would rather avoid using Options for now...
+  }
 }
--- src/test/scala/ar/com/allentiak/merging_intervals/IntervalsTest.scala
+++ src/test/scala/ar/com/allentiak/merging_intervals/IntervalsTest.scala
@@ -3,14 +3,22 @@ package ar.com.allentiak.merging_intervals
 import org.scalatest.FunSpec
 
 class IntervalTest extends FunSpec {
+
   describe("Two Intervals I1(a,b) and I2(c,d)") {
+
     it("can be merged into I3(i,j) if (a >= c && b <= d) || (c >= a && d <= b), with i=min(a,c) and j=max(b,d)"){
       val I1=Interval(1,8)
       val I2=Interval(2,7)
       assert (I1.fitsInto(I2) === false)
       assert (I2.fitsInto(I1) === true)
-      val I3=I1.merge(I2)
-      assert(I3===Interval(1,8))
+      val I3=I1.mergeWith(I2)
+      assert(I3 === Interval(1,8))
+
+      val I4=Interval(28,96)
+      val I5=I4.mergeWith(I2)
+      assert(I5 === null)
     }
+
   }
+
 }
