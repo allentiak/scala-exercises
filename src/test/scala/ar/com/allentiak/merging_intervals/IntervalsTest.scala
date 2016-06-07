27d961cde0783e6b84ddb37a7254893b757fcfaa
Leandro Doctors <ldoctors@gmail.com>
2016-06-07 06:40:06 -0300

    Refactoring

--- src/test/scala/ar/com/allentiak/merging_intervals/IntervalsTest.scala
+++ src/test/scala/ar/com/allentiak/merging_intervals/IntervalsTest.scala
@@ -13,19 +13,19 @@ class IntervalTest extends FunSpec {
           // As we will be continously adding elements to the accummulator, a stack should be the most efficient way to handle insertions.
           // However, as the Stack class has been deprecated since Scala 2.11.0, we will prepend a list.
           // (See: http://www.scala-lang.org/api/current/#scala.collection.immutable.Stack)
-          if (OriginalList.isEmpty) AccumStack.reverse
-          else {
-            if (AccumStack.isEmpty)
-            // for the first element of the list
-             consolidateAux(OriginalList.tail,OriginalList.head::AccumStack)
-            else {
-              // if intervals overlap (potential refactoring point - see below)
-              if (OriginalList.head.lowerLimit <= AccumStack.head.higherLimit)
+          OriginalList match
+          {
+            case Nil => AccumStack.reverse
+            case ol:List[Interval] =>
+           {
+             // if we are not dealing with the first element of ol and if there is overlapping...
+             // (point of potential refactoring - see below)
+            if ((!AccumStack.isEmpty) &&  (ol.head.lowerLimit <= AccumStack.head.higherLimit))
                 // replace the head of the stack with the merged interval
-                consolidateAux(OriginalList.tail, OriginalList.head.mergeWithOverlapping(AccumStack.head)::AccumStack.tail)
+                consolidateAux(ol.tail, ol.head.mergeWithOverlapping(AccumStack.head)::AccumStack.tail)
               else
                 // add the new element to the stack
-                consolidateAux(OriginalList.tail,OriginalList.head::AccumStack)
+                consolidateAux(ol.tail,ol.head::AccumStack)
             }
           }
         }
@@ -36,11 +36,14 @@ class IntervalTest extends FunSpec {
 
       val L2 = List(Interval(8,18), Interval(1,2))
 
+      val L1S=L1.sortBy(_.lowerLimit)
+      val L2S=L2.sortBy(_.lowerLimit)
+
       // Solution proof:
       // (we consider two successive intervals do not allow  merging; if we did otherwise, the "consolidate" function could be refactored by adding as a parameter a function that would define de comparison operation used to determine the existence of overlapping)
-      assert(consolidate(L1.sortBy(_.lowerLimit)) === List(Interval(1,2), Interval(3,5), Interval(6,20)))
+      assert(consolidate(L1S) === List(Interval(1,2), Interval(3,5), Interval(6,20)))
 
-      assert(consolidate(L2.sortBy(_.lowerLimit)) === L2.sortBy(_.lowerLimit))
+      assert(consolidate(L2S) === L2S)
     }
 
   }
