2772c72d96b3e7fe8c37be98f73095769dbc4fe1
Leandro Doctors <ldoctors@gmail.com>
2016-06-06 23:12:49 -0300

    Interval: add function fitsInto

--- src/main/scala/ar/com/allentiak/merging_intervals/Intervals.scala
+++ src/main/scala/ar/com/allentiak/merging_intervals/Intervals.scala
@@ -1,3 +1,6 @@
 package ar.com.allentiak.merging_intervals
 
 case class Interval(lowerLimit:Int,higherLimit:Int)
+{
+  def fitsInto(other:Interval):Boolean=(this.lowerLimit >= other.lowerLimit && this.higherLimit <= other.higherLimit)
+}
